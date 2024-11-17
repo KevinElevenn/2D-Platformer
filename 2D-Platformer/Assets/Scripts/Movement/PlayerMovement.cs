41d884483d2672f586e2b70c7cf5557ccfa3a1b5
Kevin <gmodgame258@gmail.com>
2024-11-16 21:22:00 -0800

    added grounded check and jump counter with working double jump

--- 2D-Platformer/Assets/Scripts/Movement/PlayerMovement.cs
+++ 2D-Platformer/Assets/Scripts/Movement/PlayerMovement.cs
@@ -13,6 +13,10 @@ public class PlayerMovement : MonoBehaviour
     // idk why our move is in a float it works i guess,
     private float _move;
     [SerializeField] private float _moveSpeed = 2f;
+    private bool isGrounded;
+    private int _jumpCount;
+    [SerializeField] private int _maxJumps = 2;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -31,10 +35,33 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(_move * _moveSpeed, rb.velocity.y);
 
         // will check everframe for the jump input
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && _jumpCount < _maxJumps) // checks the jump count to allow fore double jump
         {
-            // rb is our player's Rigidbody and we are appling a force to it in the x axis using the _jumpForce float to multiply it to make the player move up
-            rb.AddForce(new Vector2(rb.velocity.x, _jumpForce));
+            // Apply a force to the Rigidbody2D to make the player jump
+            rb.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse); // im pretty sure the 0 means the direction of the jump
+            ++_jumpCount; // adds 1 to jump count
+        }
+    }
+
+    // uses ColliderEnter to update grounded bool to true and resets jump count when grounded
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == ("Ground"))
+        {
+            Debug.Log("Grounded");
+            isGrounded = true;
+            Debug.Log("JumpsReset");
+            _jumpCount = 0;
+        }
+    }
+
+    // uses the exit of the collider to update Grounded bool to fales
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == ("Ground"))
+        {
+            Debug.Log("Ungrounded");
+            isGrounded = false;
         }
     }
 }
