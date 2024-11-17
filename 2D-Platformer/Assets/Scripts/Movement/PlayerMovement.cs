6dd249de70f77547eaa202d4e169f0af22fbb8e3
Kevin Johnson-Wilson <gmodgame258@gmail.com>
2024-11-16 19:03:56 -0800

    UPDATED PLAYER MOVEMENT SCRIPT TO ALLOWFOR JUMPING USING THE SPACE BAR

--- 2D-Platformer/Assets/Scripts/Movement/PlayerMovement.cs
+++ 2D-Platformer/Assets/Scripts/Movement/PlayerMovement.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    private Rigidbody2D rb;
+    [SerializeField] private float _jumpForce = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, _jumpForce));
+        }
     }
 }
